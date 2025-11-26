40b20c06efeaf07f0b1e8434ba32a6bc0ac725c3
Andrey Baranchik <ab@MacBookAir.lan>
2025-11-25 16:31:59 -0800

    Use dangerouslySetInnerHTML to inject Weedmaps embed script

--- src/components/StoreLocator.js
+++ src/components/StoreLocator.js
@@ -3,21 +3,20 @@
 import styles from './StoreLocator.module.css';
 
 export default function StoreLocator() {
+    const embedScript = `
+    <div id="wm-retailers-embed"></div>
+    <script src="https://bearlabs.wm.store/static/js/retailers-embed.js"></script>
+  `;
+
     return (
         <section id="stores" className={styles.section}>
             <h2 className={styles.heading}>FIND BEAR LABS</h2>
             <p className={styles.subheading}>Locate Bear Labs products at a retailer near you</p>
 
-            <div className={styles.buttonContainer}>
-                <a
-                    href="https://bearlabs.wm.store"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.storeButton}
-                >
-                    View Store Locator
-                </a>
-            </div>
+            <div
+                className={styles.mapContainer}
+                dangerouslySetInnerHTML={{ __html: embedScript }}
+            />
         </section>
     );
 }
