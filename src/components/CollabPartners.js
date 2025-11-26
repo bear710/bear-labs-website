eafd416e436c73c7d6b9ceceec3ed02f02b7d2cf
Andrey Baranchik <ab@Andreys-MacBook-Air.local>
2025-11-26 10:52:03 -0800

    Add remaining 5 partner logos to Collab Partners section

--- src/components/CollabPartners.js
+++ src/components/CollabPartners.js
@@ -3,10 +3,15 @@ import styles from './CollabPartners.module.css';
 export default function CollabPartners() {
     const partners = [
         { id: 1, name: "Tychee Farms", image: "/images/partners/tychee-farms.png" },
-        { id: 2, name: "Partner", image: "/images/partners/snake-p.png" },
+        { id: 2, name: "Snake P", image: "/images/partners/snake-p.png" },
         { id: 3, name: "Motley Terpz", image: "/images/partners/motley-terpz.png" },
         { id: 4, name: "Four Fathers", image: "/images/partners/four-fathers.png" },
         { id: 5, name: "Hogwash Pharms", image: "/images/partners/hogwash-pharms.png" },
+        { id: 6, name: "Casa Flor", image: "/images/partners/casa-flor.png" },
+        { id: 7, name: "Coastal Sun", image: "/images/partners/coastal-sun.png" },
+        { id: 8, name: "Canyon Creek", image: "/images/partners/canyon-creek.png" },
+        { id: 9, name: "Booney Acres", image: "/images/partners/booney-acres.png" },
+        { id: 10, name: "Chameleon", image: "/images/partners/chameleon.png" },
     ];
 
     return (
